67ae7a637957f233a9826bd45132f550f3f8cfa4
Bohdan Mamchura <mazdeyf@gmail.com>
2022-11-10 18:28:36 +0100

    project starter

---
+++ src/main/java/com/mamchura/onlinestore/OnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.mamchura.onlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreApplication.class, args);
+	}
+
+}
